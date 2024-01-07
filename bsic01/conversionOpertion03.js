1599626636ef456641eab93dcdc798a35545a4dc
Ramesh <139034735+Ramesh087@users.noreply.github.com>
2024-01-07 17:13:14 +0000

    new conversion

--- bsic01/conversionOpertion03.js
+++ bsic01/conversionOpertion03.js
@@ -1,21 +1,21 @@
 let score = "33abc"
 
-console.log(typeof score);
+// console.log(typeof score);
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
-console.log(valueInNumber);//NaN "not a number" special type when converted value are not number
+// console.log(valueInNumber);//NaN "not a number" special type when converted value are not number
 // "33" => 33
 //true => 1;false => 0
 // if we have null/undefinded at 3abc we get 0/NaN
 
 let isLoggedIn =1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(isLoggedIn);
+// console.log(isLoggedIn);
 
 // 1 => true ; 0=> false
 // ""=> false
@@ -26,5 +26,34 @@ let someNumber =33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log( typeof stringNumber);
+// console.log(stringNumber);
+// console.log( typeof stringNumber);
+
+
+
+// *********************Operation *****************************
+
+let value =3 
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);//4
+// console.log(2-2);//0
+// console.log(2*2);//4
+// console.log(2**3);//8
+// console.log(2/2);//1
+// console.log(2%2);//0 remainder
+
+let str1 = "hello"
+let str2 = "ramesh"
+let str3 = str1+str2
+//console.log(str3);
+
+
+// console.log("1"+2);//12
+// console.log(1+"2");//12
+// console.log("1"+2+2);//122
+// console.log(1+2+"2");//32
+// console.log(true);//true
+// console.log(+true);//1
+//console.log(+"");//0
