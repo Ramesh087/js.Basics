0402a4dd8ce4289dc4c68a2be7359fea0ac37d6c
Ramesh <139034735+Ramesh087@users.noreply.github.com>
2024-01-07 17:42:48 +0000

    datatype summary

---
+++ 01bsic/04comparison.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+
+// console.log(null>0);// false
+// console.log(null==0);//false
+// console.log(null>=0);//true    the reason is that an equality check == and comparison > < >= <= work differently  compersion convert null to a number , treating it as 0.
+
+// undefined give all three compersaion false but work as given reason
+
+// strict check "==="
+console.log("2"===2);//false check datatype and value both
---
+++ 01bsic/datatypes-summary.js
@@ -0,0 +1,19 @@
+// primitive
+
+// 7 types : String , Number, Boolean,null,undefined,symbol,BigInt
+
+
+
+
+// reference(non primitive)
+
+// Array,Objects,Function
+const heros = [ "shaktiman","nagraj", "doga"];
+let myobj ={
+    name: "ramesh",
+    age:20,
+}
+
+const myfunc =function(){
+    console.log("hello ramesh");
+}
