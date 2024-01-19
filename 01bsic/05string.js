13f4f6e353f6a4fec3b45dcb9306690cdb8abd9e
Ramesh <139034735+Ramesh087@users.noreply.github.com>
2024-01-19 19:58:40 +0000

    string

---
+++ 01bsic/05string.js
@@ -0,0 +1,14 @@
+const name ="ramesh"
+const repocount = 50
+
+//console.log(name+repocount+" value")
+
+//console.log(`hello my world ${name} and my repo ${repocount}`);// modern way called string interpolation
+
+
+const gameName= new String('ramesh')
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+console.log(gameName.slice(-7,4))//s    rame
+
+// study methods from mdn
