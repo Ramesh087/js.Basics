9c69e8dc8286c7d7bb2b829d55aa675379173f50
Ramesh <139034735+Ramesh087@users.noreply.github.com>
2024-01-19 19:14:48 +0000

    mmory with data

--- 01bsic/datatypes-summary.js
+++ 01bsic/datatypes-summary.js
@@ -17,3 +17,39 @@ let myobj ={
 const myfunc =function(){
     console.log("hello ramesh");
 }
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++
+
+// memory
+//stack(primitivte type data)  ....... in stack memory we get copy of data
+// heap( non primitive)       ......... in heap memeory we get references of data
+
+
+
+// stack example
+let userid = "ramesh@gmail.com"
+let rough = userid
+//console.log(rough)
+ 
+rough="thg@gmail.com"
+console.log(userid)
+console.log(rough)
+
+
+
+// heap example
+let userone={
+    email:"ramesh@gmail.com",
+    mob:2222,
+}
+
+
+
+let usertwo = userone
+console.log(usertwo.email)
+usertwo.email = "potliy@gmail.com"
+
+console.log(userone.email)
+console.log(usertwo.email)
