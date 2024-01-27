8e7c3b125da3f4d97f89ccc5476a2f98a5f258c3
Ramesh <139034735+Ramesh087@users.noreply.github.com>
2024-01-27 10:12:30 +0000

    array part 1

---
+++ 01bsic/arrays.js
@@ -0,0 +1,24 @@
+//arrays
+
+const myarr = [0,1,2,3,4,5]
+
+// array methods
+
+// myarr.push(4)
+// myarr.push(7)
+// myarr.pull()
+
+// myarr.unshift(9)// start me add karta h
+
+// myarr.shift()//start se delete element
+// console.log(myarr)
+
+// const newarr = myarr.join()
+// console.log(newarr)
+// console.log(typeof newarr)//string
+
+
+// slice , splice
+
+// slice ---original array ko nhi pareshan karta h
+// splice ---original array me se delete kar deta h
