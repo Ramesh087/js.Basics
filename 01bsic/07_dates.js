e1a639d65e97805dccbb21d74247a83079f2da94
Ramesh <139034735+Ramesh087@users.noreply.github.com>
2024-01-27 09:51:35 +0000

    dates basics

---
+++ 01bsic/07_dates.js
@@ -0,0 +1,22 @@
+// Dates
+
+let myDates = new Date()
+// console.log(myDates.toString())
+// console.log(myDates.toISOString())
+// console.log(myDates.toJSON())
+// console.log(myDates.toLocaleDateString())
+// console.log(myDates.toLocaleString())
+console.log(typeof myDates);
+
+// let mycreateddate = new Date (2023,0,23)
+// let mycreateddate = new Date (2023,0,23,5,3)
+let mycreateddate = new Date ("01-14-2024")
+//console.log(mycreateddate.toLocaleString())
+
+let myTimestamp = Date.now()
+// console.log(myTimestamp)
+
+// console.log(mycreateddate.getTime())
+
+//.getMonth
+//.getDay etc
