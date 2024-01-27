cda5567bb702310287f1c9ae701b2bde3e0ecdb2
Ramesh <139034735+Ramesh087@users.noreply.github.com>
2024-01-27 09:22:37 +0000

    maths js complete git push origin main

---
+++ 01bsic/06nums_maths.js
@@ -0,0 +1,26 @@
+// const score = 400
+// console.log(score);//100
+
+// const balance = new Number(100)//dedkitly define kar sakte h datatype
+// console.log(balance);//[Number: 100]
+
+// console.log(balance.toString());//100
+// console.log(balance.toString().length)//3
+// console.log(balance.toFixed(2))//100.00
+
+// const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(3))//124
+
+// const hundereds = 1000000
+// console.log(hundereds.toLocaleString('en-In'))//10,00,000
+
+
+// +++++++++++++++++++++++++++ Maths
+
+// console.log(Math)
+// console.log(Math.abs(-4))// absoulute value 4
+// console.log(Math.round(4.3))//4
+// console.log(Math.ceil(4.2))//5
+// console.log(Math.floor(4.9))//4
+
+console.log(Math.floor(Math.random()*10))
