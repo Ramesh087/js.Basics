c525bfa6dccd775eea516c61d2c41f750c78a7c3
Ramesh <139034735+Ramesh087@users.noreply.github.com>
2024-01-29 09:29:50 +0000

    objects t

--- 02basic/03_objects.js
+++ 02basic/03_objects.js
@@ -6,8 +6,13 @@
 
 
 // object literals
+
+const mySym = Symbol("key1")
+
 const jsUser = {
     name: "Ramesh",
+    [mySym] :"mykey1",// tyeof mysym is string not a symbol in this object  symbol me use karne ke liye "[]"isme likho
+    "full name":"Ramesh P",
     age: 18,
     location: "jaipur",
     email : "ramesh@2003",
@@ -18,3 +23,16 @@ const jsUser = {
 
 console.log(jsUser.email)
 console.log(jsUser["email"])
+console.log(jsUser["full name"]);//we don't acess the full name from '.'
+console.log(jsUser[mySym]);// isme ese hi Acess kar sakte h
+jsUser.email ="ramesgpoou@200"
+Object.freeze(jsUser)
+jsUser.email= "fyrhfhfxxfbcfg@55"// object is free we can't overrie the email
+console.log(jsUser);
+
+jsUser.greeting = function() {
+    console.log("hello js user");
+}
+
+console.log(jsUser.gr);
+
