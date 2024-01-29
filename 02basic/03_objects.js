bfa8153218ef5f08e4e13931f5542bb76474106e
Ramesh <139034735+Ramesh087@users.noreply.github.com>
2024-01-29 09:23:31 +0000

    objects

--- 02basic/03_objects.js
+++ 02basic/03_objects.js
@@ -1,1 +1,20 @@
-//
+//object ko two type se declare kar sakte h  literal and constructor 
+
+//singelton == literal ki trah declare karte h to singleton nhi banta h
+// object.create
+
+
+
+// object literals
+const jsUser = {
+    name: "Ramesh",
+    age: 18,
+    location: "jaipur",
+    email : "ramesh@2003",
+    isLoggedIn: false,
+    lastlogindays : ["Monday", "Saturday"]
+
+}
+
+console.log(jsUser.email)
+console.log(jsUser["email"])
