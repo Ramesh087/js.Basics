7ac13ca953221eb71b6ad984a8ca7087015dc0df
Ramesh <139034735+Ramesh087@users.noreply.github.com>
2024-02-03 18:46:20 +0000

    object2

---
+++ 02basic/04_object.js
@@ -0,0 +1,58 @@
+//const tinderUser = new Object()  //singelton (constructor) object
+const tinderUser ={}   //non singelton
+
+tinderUser["id"]= "123abc"
+tinderUser.name ="rames"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+email:"some@gmail.com",
+fullname:{
+    userfullname:{
+        firstman:"ramesh",
+        lastname:"pot"
+     }
+  }
+}
+ 
+// console.log(regularUser.fullname.userfullname.firstman);
+
+const obj1 ={1:"a",2:"b"}
+const obj2 ={3:"a",4:"b"}
+// const obj3 = {obj1,obj2}  // problem that give object ke andar object
+
+// const obj3 = Object.assign({},obj1,obj2)//obj1 aand obj 2 copy in {}
+ const obj3 ={...obj1,...obj2}// spread the objects
+
+console.log(obj3);
+
+
+
+// const users = {
+//     {
+//         id:1,
+//         email:"r@gmail.com"
+//     },
+//     {
+//         id:1,
+//         email:"r@gmail.com"
+//     },
+//     {
+//         id:1,
+//         email:"r@gmail.com"
+//     },
+//     {
+//         id:1,
+//         email:"r@gmail.com"
+//     },
+// }
+
+
+// user[1].email;
+
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));//datatype of keys is array
