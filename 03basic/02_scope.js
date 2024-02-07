dd6372acbb362dd511adb3b3bdb6706242a0f2c4
Ramesh <139034735+Ramesh087@users.noreply.github.com>
2024-02-07 12:17:24 +0000

    scope

--- 03basic/02_scope.js
+++ 03basic/02_scope.js
@@ -11,3 +11,28 @@ if(true){
 //console.log(a);
 //console.log(b);
 //console.log(c);
+
+
+function one() {
+    const username ="Ramesh"
+    function two(){
+        const website ="youtube"
+    }
+    //console.log(website);
+    two()
+}
+//one()
+
+//++++++++++++++++++++++++++interesting
+
+
+function addone(num) {
+    return num+1
+}
+addone(5)//isko kahi pe bhi rakh sakte h
+
+const addtwo = function(num){
+    return num+2
+}
+
+addtwo(5)//hoisting  //isko addtwo ke uper nhi rakh sakte acess nhi kar payega
