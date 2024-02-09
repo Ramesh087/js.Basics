2d6c38dff21a8012e5cd51bb2380bfcdc9b6db36
Ramesh <139034735+Ramesh087@users.noreply.github.com>
2024-02-09 16:59:45 +0000

    loop and break and continue

---
+++ 05_iteration/one.js
@@ -0,0 +1,21 @@
+// for
+
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    console.log(element);
+    
+}
+
+
+// break and continue
+// while and dowhile
+let index = 0
+while (index<10) {
+    console.log(`value of index ${index}`);
+    index = index+2
+}
+let score = 1
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score<=10);
